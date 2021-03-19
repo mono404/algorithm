af103cffba0f8c3395e5d3a58b9b1cb38dbe879f
김원호 <57743163+mono404@users.noreply.github.com>
2021-03-19 21:28:41 +0900

    Implement with BFS

--- Baekjoon/7576_BFS.cpp
+++ Baekjoon/7576_BFS.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 #include <queue>
+#include <set>
 
 using namespace std;
 
 void BFS();
 
-int n, m, ans;
-int arr[1002][1002];
-int check[1002][1002];
-queue<pair<int, int>> q;
+int n, m, h, ans;
+int arr[102][102][102];
+int check[102][102][102];
+queue<pair<int, pair<int, int>>> q;
 
-int dx[4] = { 0, 1, 0, -1 };
-int dy[4] = { 1, 0, -1, 0 };
+int dx[6] = { 0, 1, 0, -1, 0, 0 };
+int dy[6] = { 1, 0, -1, 0, 0, 0 };
+int dz[6] = { 0, 0, 0, 0, 1, -1 };
 
 int main(void)
 {
@@ -20,49 +22,44 @@ int main(void)
 
 	cin >> n;
 	cin >> m;
+	cin >> h;
 
-	for (int i = 1; i <= m; i++)
+	for (int k = 1; k <= h; k++)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int i = 1; i <= m; i++)
 		{
-			cin >> arr[i][j];
-			if (arr[i][j] == -1)
-				check[i][j] = -1;
-		}
-	}
-
-	for (int i = 1; i <= m; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (arr[i][j] == 1)
+			for (int j = 1; j <= n; j++)
 			{
-				q.push(pair<int, int>(i, j));
-				check[i][j] = 1;
-				ans = 1;
+
+				cin >> arr[k][i][j];
+				if (arr[k][i][j] == -1)
+					check[k][i][j] = -1;
+				else if (arr[k][i][j] == 1)
+				{
+					q.push(pair<int, pair<int, int>>(k, make_pair(i, j)));
+					check[k][i][j] = 1;
+					ans = 1;
+				}
+
 			}
 		}
 	}
 
 	BFS();
 
-	for (int i = 1; i <= m; i++)
+	for (int k = 1; k <= h; k++)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int i = 1; i <= m; i++)
 		{
-			cout << check[i][j] << "\t";
-		}
-		cout << endl;
-	}
-
-	for (int i = 1; i <= m; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (check[i][j] == 0)
+			for (int j = 1; j <= n; j++)
 			{
-				cout << "-1" << endl;
-				return 0;
+
+				if (check[k][i][j] == 0)
+				{
+					cout << "-1" << endl;
+					return 0;
+				}
+
 			}
 		}
 	}
@@ -72,27 +69,30 @@ int main(void)
 
 void BFS()
 {
-	int cur_y = q.front().first;
-	int cur_x = q.front().second;
+	int cur_z = q.front().first;
+	int cur_y = q.front().second.first;
+	int cur_x = q.front().second.second;
 
 	while (!q.empty())
 	{
-		cur_y = q.front().first;
-		cur_x = q.front().second;
+		cur_z = q.front().first;
+		cur_y = q.front().second.first;
+		cur_x = q.front().second.second;
 		q.pop();
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 6; i++)
 		{
 			int next_y = cur_y + dy[i];
 			int next_x = cur_x + dx[i];
+			int next_z = cur_z + dz[i];
 
-			if (arr[next_y][next_x] == 0 && check[next_y][next_x] == 0)
+			if (arr[next_z][next_y][next_x] == 0 && check[next_z][next_y][next_x] == 0)
 			{
-				check[next_y][next_x] = check[cur_y][cur_x] + 1;
-				q.push(pair<int, int>(next_y, next_x));
+				check[next_z][next_y][next_x] = check[cur_z][cur_y][cur_x] + 1;
+				q.push(pair<int, pair<int, int>>(next_z, make_pair(next_y, next_x)));
 
-				if (ans < check[next_y][next_x])
-					ans = check[next_y][next_x];
+				if (ans < check[next_z][next_y][next_x])
+					ans = check[next_z][next_y][next_x];
 			}
 		}
 	}
