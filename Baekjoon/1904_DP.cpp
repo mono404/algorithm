ea53136844f69d6457643eecbe91786dd31cfd54
김원호 <57743163+mono404@users.noreply.github.com>
2021-03-31 19:50:59 +0900

    implement with DP

---
+++ Baekjoon/1904_DP.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int n;
+int dp[1000001];
+
+int main(void)
+{
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
+
+	for (int i = 4; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+		dp[i] = dp[i] % 15746;
+	}
+
+	cout << dp[n] << endl;
+}
